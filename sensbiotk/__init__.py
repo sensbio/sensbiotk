e86f8e97fe137b348ce6993e4160508a62628d34
Roger Pissard-Gibollet <roger.pissard@inria.fr>
2015-02-09 11:55:53 +0100

    dev

--- sensbiotk/__init__.py
+++ sensbiotk/__init__.py
@@ -2,7 +2,7 @@
 _version_major = 2
 _version_minor = 0
 _version_micro = ''
-_version_extra = 'svn-1250'
+_version_extra = 'dev'
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 __version__ = "%s.%s.%s%s" % (_version_major,
